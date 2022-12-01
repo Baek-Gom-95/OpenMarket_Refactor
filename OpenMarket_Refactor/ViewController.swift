9131866eb1612f2f186391a6374f5a5e75867033
BaekGom <jihunmm02@gmail.com>
2022-12-01 22:04:55 +0900

    feat: 세그먼트 구현

--- OpenMarket_Refactor/ViewController.swift
+++ OpenMarket_Refactor/ViewController.swift
@@ -8,25 +8,81 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var containerView: UIView = {
+        let container = UIView()
+        container.backgroundColor = .yellow
+        container.translatesAutoresizingMaskIntoConstraints = false
+        return container
+    }()
+    
+    private var segmentControl: UISegmentedControl = {
+        let segment = UISegmentedControl()
+        segment.selectedSegmentTintColor = .clear
+        segment.setBackgroundImage(UIImage(), for: .normal, barMetrics: .default)
+        segment.setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+        segment.insertSegment(withTitle: "List", at: 0, animated: true)
+        segment.insertSegment(withTitle: "Grid", at: 1, animated: true)
+        segment.selectedSegmentIndex = 0
+        
+        segment.setTitleTextAttributes([
+            NSAttributedString.Key.foregroundColor: UIColor.black,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .regular)
+        ], for: .normal)
+        
+        segment.setTitleTextAttributes([
+            NSAttributedString.Key.foregroundColor: UIColor.systemBlue,
+            NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16, weight: .bold)
+        ], for: .selected)
+        
+        segment.addTarget(ViewController.self, action: #selector(changeSegment), for: .valueChanged)
+        
+        segment.translatesAutoresizingMaskIntoConstraints = false
+        return segment
+    }()
+    
+    private lazy var underLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBlue
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var leadingDistance: NSLayoutConstraint = {
+        return underLineView.leadingAnchor.constraint(equalTo: segmentControl.leadingAnchor)
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        checkNetWork()
+        configure()
     }
-
-    
-    func checkNetWork() {
-        let networkManager = NetworkManager()
-        
-        networkManager.checkHealth(by: URLCollection.hostURL + URLCollection.healthChecker.string, completion: { (response) in
-            switch response {
-            case .success(let data):
-                print(data)
-            case .failure(let data):
-                print(data)
-            }
-        })
-    }
-
 }
 
+extension ViewController {
+    func configure() {
+        self.view.addSubview(containerView)
+        self.containerView.addSubview(segmentControl)
+        self.containerView.addSubview(underLineView)
+        
+        NSLayoutConstraint.activate([
+            containerView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            containerView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            containerView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            containerView.heightAnchor.constraint(equalToConstant: 40),
+            
+            segmentControl.topAnchor.constraint(equalTo: containerView.topAnchor),
+            segmentControl.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            segmentControl.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            segmentControl.centerXAnchor.constraint(equalTo: containerView.centerXAnchor),
+            
+            underLineView.bottomAnchor.constraint(equalTo: segmentControl.bottomAnchor),
+            underLineView.heightAnchor.constraint(equalToConstant: 5),
+            leadingDistance,
+            underLineView.widthAnchor.constraint(equalTo: segmentControl.widthAnchor, multiplier: 1 / CGFloat(segmentControl.numberOfSegments))
+        ])
+    }
+    
+    @objc private func changeSegment() {
+        
+    }
+}
